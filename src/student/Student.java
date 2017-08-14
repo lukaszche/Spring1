b56d9534890fb639c70b1ddac0d280f1f6bceded
Lukasz <ljablona@gmail.com>
2017-08-14 14:10:28 +0200

    CommmitSpring1

---
+++ src/student/Student.java
@@ -0,0 +1,19 @@
+package student;
+
+public class Student {
+
+	private String name;
+	
+	public String getName(){
+		return name;
+	}
+	public void setName(String n){
+		name=n;
+	}
+	
+	public void displayInfo(){
+		System.out.println("Hello "+name);
+	}
+	
+	
+}
